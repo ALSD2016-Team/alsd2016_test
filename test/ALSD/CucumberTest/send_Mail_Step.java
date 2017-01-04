8988f1fff1336e7e63e9f55f47d61baf7c9f7681
Dopiz <r.j.liu0903@gmail.com>
2017-01-04 22:10:48 +0800

    Test.

--- test/ALSD/CucumberTest/send_Mail_Step.java
+++ test/ALSD/CucumberTest/send_Mail_Step.java
@@ -19,7 +19,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class sendMailStep {
+public class send_Mail_Step {
 	
 	WebDriver driver;
 	private static String OS = System.getProperty("os.name").toLowerCase();
